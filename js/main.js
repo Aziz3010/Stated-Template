d3614b72f1cca09b58435b29bdb86ab5301d44ab
unknown <a.abdelazizg@gmail.com>
2021-08-15 23:31:25 +0200

    add new section

--- js/main.js
+++ js/main.js
@@ -56,149 +56,32 @@ $("#Navbar .links ul li a,#Navbar .logoText a").click(function(e){
     $("body , html").animate({scrollTop:sectionOffset},1500);
 });
 
-// testimonials
+// owlCarousel
 
-var newsBox = document.getElementById("newsBox");
-var control1 = document.getElementById("control1");
-var control2 = document.getElementById("control2");
-var control3 = document.getElementById("control3");
-
-
-control1.addEventListener("click",function(){
-    newsBox.style.transform = "translateX(800px)";
-    control1.classList.add("active");
-    control2.classList.remove("active");
-    control3.classList.remove("active");
-});
-
-control2.addEventListener("click",function(){
-    newsBox.style.transform = "translateX(0px)";
-    control1.classList.remove("active");
-    control2.classList.add("active");
-    control3.classList.remove("active");
-});
-
-control3.addEventListener("click",function(){
-    newsBox.style.transform = "translateX(-800px)";
-    control1.classList.remove("active");
-    control2.classList.remove("active");
-    control3.classList.add("active");
-});
-
-
-
-
-
-
-
-
-
-
-// ////////////////////
-
-// slider
-
-var sliderImages = Array.from(document.querySelectorAll(".slider-container img"));
-var getImagerNumber = sliderImages.length;
-var currentSlide = 5;
-var slide_Number_Box = document.getElementById("slide-number");
-
-var PreviousBTN = document.getElementById("prev");
-var NextBTN = document.getElementById("next");
-
-
-PreviousBTN.addEventListener("click",function(){
-    PrevSlide();
-})
-
-
-NextBTN.addEventListener("click",function(){
-    nextSlide();
-})
-
-
-function nextSlide(){
-    if(NextBTN.classList.contains("disabled")){
-        return false;
-    } else{
-        currentSlide++;
-        console.log(currentSlide);
-        checker()
+  var owl = $('.owl-carousel');
+  owl.owlCarousel({
+    loop:true,
+    margin:25,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+        },
+        800:{
+            items:2,
+        },
+        1000:{
+            items:4,
+        }
     }
-}
+  });
 
-function PrevSlide(){
-    if(PreviousBTN.classList.contains("disabled")){
-        return false;
-    } else{
-        currentSlide--;
-        console.log(currentSlide);
-        checker()
-    }
-}
+  // Go to the next item
+  $('#Properties .indecator #Next').click(function() {
+      owl.trigger('next.owl.carousel');
+  })
 
-// create ul + li
-
-var paginationElement = document.createElement("ul");
-paginationElement.setAttribute("id","pagination-ul");
-
-for (var i=0 ; i<getImagerNumber ; i++){
-    var listsElements = document.createElement("li");
-    
-    listsElements.setAttribute("data-index", i);
-
-    listsElements.appendChild(document.createTextNode(i+1));
-
-    paginationElement.appendChild(listsElements);
-
-}
-
-var indicators = document.getElementById("indicators");
-indicators.appendChild(paginationElement);
-
-var paginationCreatedUl = document.getElementById("pagination-ul");
-var paginationArray = Array.from(document.querySelectorAll("#pagination-ul li"));
-
-
-for(var i=0 ; i<getImagerNumber ; i++){
-    paginationArray[i].addEventListener("click",function(){
-        currentSlide = parseInt(this.getAttribute("data-index")) + 1;
-        checker()
-    })
-}
-
-function checker(){
-    slide_Number_Box.textContent = `Silde #${currentSlide} of ${getImagerNumber}`;
-
-    removeActives()
-
-    sliderImages[currentSlide - 1].classList.add("active");
-    
-    paginationCreatedUl.children[currentSlide - 1].classList.add("active");
-
-
-    if(currentSlide == 1){
-        PreviousBTN.classList.add("disabled");
-    } else  {
-        PreviousBTN.classList.remove("disabled");
-    }
-
-    if(currentSlide == getImagerNumber ){
-        NextBTN.classList.add("disabled");
-    } else  {
-        NextBTN.classList.remove("disabled");
-    }
-
-}
-checker()
-
-function removeActives(){
-    sliderImages.forEach(function(images){
-        images.classList.remove("active");
-    })
-
-    paginationArray.forEach(function(lists){
-        lists.classList.remove("active");
-    })
-}
-// ////////////////////////////////////////////////
+  // Go to the previous item
+  $('#Properties .indecator #Prev').click(function() {
+      owl.trigger('prev.owl.carousel');
+  })
